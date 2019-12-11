3bfe8a6d98b77c1069ee9b229e41189128d07ca6
IkedaYasufumi <yasufumi@dc5.so-net.ne.jp>
2019-12-11 14:52:59 +0900

    test33

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React,{ useReducer } from 'react'
+import React, { useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm.js'
 import reducer from '../reducers/index.js'
 import Events from './Events.js'
+import AppContext from '../contexts/AppContext'
+
+console.log({ AppContext })
 
 const App = () => {
 
-  const [state,dispatch] = useReducer(reducer,[])
+  const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <div className='container-fluid'>
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-
-    </div>
+    <AppContext.Provider value={'Hello, I am Yasufumi222.'}>
+      <div className='container-fluid'>
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 }
 export default App;
